3485a6fbc95fbd24a5ec795135d01d48d440fd3e
Laikaiyong <vandycklai@gmail.com>
2022-04-23 16:55:51 +0800

    Upload completed project

--- main.py
+++ main.py
@@ -1,54 +1,99 @@
-def add_time(time, duration, day=None):
-    days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
-    midday = ('PM', 'AM')
-    final_day = ""
-    final_recent = ""
+class Category:
+  def __init__(self, category_name):
+    self.name = category_name
+    self.ledger = []
 
-    time_splitted, time_period = time.split(" ")
-    time_hour, time_minute = map(int, time_splitted.split(":"))
-    added_hour, added_minute = map(int, duration.split(":"))
+  def get_balance(self):
+    total = 0
+    for item in self.ledger:
+      total += item['amount']
+
+    return total
+  
+  def deposit(self, amount, description):
+    self.ledger.append({
+      'amount': amount,
+      'description': description
+    })
+
+  def withdraw(self, amount, description="Withdrawal"):
+    if self.check_funds(amount):
+      self.ledger.append({
+        'amount': -amount,
+        'description': description
+      })
+      return True
+    else:
+      return False
+
+  def transfer(self, amount, object):
+    transferable = self.check_funds(amount)
+    if transferable:
+      self.withdraw(amount, "Transfer to " + object.name)
+      object.deposit(amount, "Transfer from " + self.name)
+      return True
+    else:
+      return False
+
+  def check_funds(self, amount):
+    if self.get_balance() >= amount:
+      return True
+    else:
+      return False
+
+  def __str__(self):
+    pattern = "*" * 12
+    full_text = pattern + self.name + pattern + "*\n"
+    for item in self.ledger:
+      full_text += item['description'][0:23].ljust(24, " ") + "%.2f\n" % item['amount']
+
+    full_text += "Total: " + str(self.get_balance())
+    return full_text
     
-    extra_hours, final_minute = divmod(time_minute + added_minute, 60)
-    cycle, final_hour = divmod(time_hour + extra_hours + added_hour, 12)
+def create_spend_chart(categories):
+  full_text = "Percentage spent by category\n"
 
-    period_index = abs(midday.index(time_period) - (cycle % 2))
-    passed_days = (period_index + cycle) // 2
+  each_total = [category.get_balance() for category in categories]
+  total = sum(each_total)
+  percentile = [ round(subtotal/total * 100, -1) for subtotal in each_total]
+  for tenths in range(10):
+    percentage = 100 - tenths * 10
+    full_text += str(percentage).rjust(3, " ") + "|" 
+    for percent in percentile:
+        if percent >= percentage:
+            full_text += " o "
+        else:
+            full_text += "   "
+    full_text += '\n'
 
-    if final_hour == 0:
-        final_hour = 12
+  full_text += " " * 4 + "-" * 9 + "\n"
+  each_name = [category.name for category in categories]
+
+  for index in range(len(max(each_name, key=len))):
+    full_text += " " * 3
+    for name in each_name:
+      try:
+        full_text += " " + name[index] + " "
+      except:
+        full_text += " " * 3
+    full_text += "\n"
     
-    if day:
-        final_day = f', {days[(days.index(day.capitalize()) + passed_days) % 7]}'
+  return full_text
 
-    if passed_days == 1:
-        final_recent = ' (next day)'
-    elif passed_days > 1:
-        final_recent = f' ({passed_days} days later)'
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
 
+print(food)
+print(clothing)
 
-    final_time = '{}:{} {}{}{}'.format(
-        final_hour,
-        str(final_minute).zfill(2),
-        midday[period_index],
-        final_day,
-        final_recent
-    )
-    return final_time
-
-print(add_time("3:00 PM", "3:10"))
-# Returns: 6:10 PM
-
-print(add_time("11:30 AM", "2:32", "Monday"))
-# Returns: 2:02 PM, Monday
-
-print(add_time("11:43 AM", "00:20"))
-# Returns: 12:03 PM
-
-print(add_time("10:10 PM", "3:30"))
-# Returns: 1:40 AM (next day)
-
-print(add_time("11:43 PM", "24:20", "tueSday"))
-# Returns: 12:03 AM, Thursday (2 days later)
-
-print(add_time("6:30 PM", "205:12"))
-# Returns: 7:42 AM (9 days later)
+print(create_spend_chart([food, clothing, auto]))
