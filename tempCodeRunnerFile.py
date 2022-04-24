f2a0c8e09d3e760c05e2ad42ca80ef591b622120
Laikaiyong <vandycklai@gmail.com>
2022-04-24 18:59:39 +0800

    Remove cache file

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-probability = experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
-print("Probability:", probability)
